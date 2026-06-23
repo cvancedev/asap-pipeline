fce932a7d7b7d02d6c25215e9c79714b62436845
wwjd12 <curtvance.dev@hotmail.com>
2026-06-23 12:29:13 -0400

    Firebase authentication and secure Firestore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHeBQRIUv4Q544TZeHjqln6dEJ8xFDpL4",
-  authDomain: "asap-pipeline.firebaseapp.com",
-  projectId: "asap-pipeline",
-  storageBucket: "asap-pipeline.firebasestorage.app",
-  messagingSenderId: "788575719840",
-  appId: "1:788575719840:web:684aea2d4df4bb799414c5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
